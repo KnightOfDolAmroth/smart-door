03181446f2b9a26adf962fb5f73fcea8b88f5955
Tremamunno Luca <luca.tremamunno@studio.unibo.it>
2018-02-02 14:50:58 +0100

    ClientHandler now flushes

--- raspi/src/consegna_4/ClientHandler.java
+++ raspi/src/consegna_4/ClientHandler.java
@@ -6,41 +6,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Collection;
 
 public class ClientHandler extends Thread {
     private final Socket socket;
     private final LogInfoProvider logInfoProvider;
     private final RoomInfoProvider roomInfoProvider;
+    private final BufferedWriter writer;
+    private final BufferedReader reader;
 
     public ClientHandler(Socket socket, LogInfoProvider logInfoProvider, RoomInfoProvider roomInfoProvider)
             throws IOException {
         this.socket = socket;
         this.logInfoProvider = logInfoProvider;
         this.roomInfoProvider = roomInfoProvider;
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 
     @Override
     public void run() {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
+        try {
             String lastMsg;
-
             do {
-                lastMsg = reader.readLine();
+                lastMsg = readFromServer();
                 switch (lastMsg) {
                 case "i":
-                    writer.write(roomInfoProvider.getRoomInfo().toString() + "\n");
+                    writeLineToServer(roomInfoProvider.getRoomInfo().toString());
                     break;
                 case "l":
-                    for (String s : logInfoProvider.getLog()) {
-                        writer.write(s + "\n");
-                    }
+                    writeLinesToServer(logInfoProvider.getLog());
                     break;
                 case "q":
-                    writer.write("Bye\n");
+                    writeLineToServer("Bye");
                     break;
                 default:
-                    writer.write("Can't understand request\n");
+                    writeLineToServer("Can't understand request");
                 }
             } while (!lastMsg.equals("q"));
         } catch (IOException e) {
@@ -48,9 +49,37 @@ public class ClientHandler extends Thread {
         }
 
         try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String readFromServer() throws IOException {
+        return reader.readLine();
+    }
+
+    private void writeLineToServer(String msg) throws IOException {
+        writer.write(msg);
+        writer.newLine();
+        writer.flush();
+    }
+
+    private void writeLinesToServer(Collection<String> msgs) throws IOException {
+        for (String s : msgs) {
+            writer.write(s);
+            writer.newLine();
+        }
+        writer.flush();
     }
 }
